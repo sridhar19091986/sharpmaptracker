eef741fc613ffe128b265efc3c0ce616c6d25c87
bruno.dunbar <bruno.dunbar@gmail.com>
2012-10-12 02:06:02 +0000

    Fixed map load bug

--- TibiaCastDownloader/Program.cs
+++ TibiaCastDownloader/Program.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.IO;
+using NDesk.Options;
 
 namespace TibiaCastDownloader
 {
@@ -14,89 +15,128 @@ namespace TibiaCastDownloader
     {
         static void Main(string[] args)
         {
-            string tibiacastClient = null;
-            if (File.Exists(@"C:\Program Files (x86)\Tibiacast\Tibiacast Client.exe"))
-                tibiacastClient = @"C:\Program Files (x86)\Tibiacast\Tibiacast Client.exe";
-            else if (File.Exists(@"C:\Program Files\Tibiacast\Tibiacast Client.exe"))
-                tibiacastClient = @"C:\Program Files\Tibiacast\Tibiacast Client.exe";
+            int startPage = 1;
+            int endPage = 10;
+            string outputFolder = @".\";
+            bool showHelp = false;
 
-            if (tibiacastClient != null)
+            var optionSet = new OptionSet()
             {
-                WebClient webClient = new WebClient();
-                Process process = Process.GetProcessesByName("Tibiacast Client").FirstOrDefault();
+                {"start-page=", "the start page.", (int v) => startPage = v},
+                {"end-page=", "the end page.", (int v) => endPage = v},
+                {"output-folder=", "the output folder", (string v) => outputFolder = v},
+                {"h|help",  "show this message and exit", v => showHelp = v != null}
+            };
 
-                var recordDirectory = System.IO.Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Tibiacast", "Recordings");
+            try
+            {
+                optionSet.Parse(args);
 
-                for (int page = 550; page <= 600; page++)
-                {
-                    Console.WriteLine("Downloading page " + page + " content.");
-                    var pageContent = webClient.DownloadString("https://www.tibiacast.com/recordings?sort=Sort%20by%20date&page=" + page);
-
-                    var matches = Regex.Matches(pageContent, @"tibiacast:recording:([\d]+)");
-                    Console.WriteLine("Found " + matches.Count + " records on page " + page + ".");
-
-                    int count = 0;
-
-                    foreach (Match match in matches)
-                    {
-                        if (process == null)
-                        {
-                            Console.WriteLine("Opening Tibiacast Client.");
-                            ProcessStartInfo startInfo = new ProcessStartInfo();
-                            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                            startInfo.FileName = tibiacastClient;
-                            process = Process.Start(startInfo);
-                            process.WaitForInputIdle();
-                        }
-
-                        var recordId = match.Groups[1].Value;
-
-                        if (File.Exists(Path.Combine(recordDirectory, recordId + ".recording")))
-                        {
-                            Console.WriteLine("Skiping record " + recordId + ".");
-                            continue;
-                        }
-
-                        var recordCount = Directory.GetFiles(recordDirectory, "*.recording").Length;
-                        var timeoutTime = DateTime.Now.AddSeconds(20);
-
-                        Console.WriteLine("Sending record " + recordId + " to Tibiacast Client.");
-
-                        Process.Start(tibiacastClient, "tibiacast:recording:" + recordId);
-
-                        while (timeoutTime > DateTime.Now)
-                        {
-                            if (Directory.GetFiles(recordDirectory, "*.recording").Length > recordCount)
-                                break;
-
-                            Thread.Sleep(500);
-                        }
-
-                        count++;
-                    }
-
-                    if (count > 0)
-                    {
-                        Console.WriteLine("Waiting a little before moving to next page.");
-                        Thread.Sleep(2000);
-
-                        if (process != null && !process.HasExited)
-                        {
-                            Console.WriteLine("Killing the Tibiacast Client before moving to next page.");
-                            process.Kill();
-                            process = null;
-                        }
-                    }
-                }
-
-                Console.WriteLine("All pages were processed. Press any key to exit...");
+                if (showHelp)
+                    Console.WriteLine("usage: tibiacast-downloader.exe --start-page=1 --end-page=10 --output-folder=\"C:\\recordings\"");
+                else
+                    DownloadRecordings(startPage, endPage, outputFolder);
             }
-            else
+            catch (Exception e)
+            {
+                Console.Write("tibiacast-downloader: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `tibiacast-downloader --help' for more information.");
+            }
+        }
+
+        static void DownloadRecordings(int startPage, int endPage, string outputFolder)
+        {
+            if (!Directory.Exists(outputFolder))
+            {
+                Console.WriteLine("Output folder does not exists or is not accessible.");
+                return;
+            }
+
+            string tibiacastClientFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), @"Tibiacast\Tibiacast Client.exe");
+            if (!File.Exists(tibiacastClientFileName))
             {
                 Console.WriteLine("Tibiacast client not found.");
+                return;
             }
 
-            Console.ReadKey();
+
+
+            var recordDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"Tibiacast\Recordings");
+
+            if (!Directory.Exists(recordDirectory))
+            {
+                Console.WriteLine("Tibiacast client recording directory not found.");
+                return;
+            }
+
+            WebClient webClient = new WebClient();
+            Process process = Process.GetProcessesByName("Tibiacast Client").FirstOrDefault();
+
+            for (int page = startPage; page <= endPage; page++)
+            {
+                Console.WriteLine("Downloading page " + page + " content.");
+                var pageContent = webClient.DownloadString("https://www.tibiacast.com/recordings?sort=Sort%20by%20date&page=" + page);
+
+                var matches = Regex.Matches(pageContent, @"tibiacast:recording:([\d]+)");
+                Console.WriteLine("Found " + matches.Count + " records on page " + page + ".");
+
+                int count = 0;
+
+                foreach (Match match in matches)
+                {
+                    if (process == null)
+                    {
+                        Console.WriteLine("Opening Tibiacast Client.");
+                        ProcessStartInfo startInfo = new ProcessStartInfo();
+                        startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        startInfo.FileName = tibiacastClientFileName;
+                        process = Process.Start(startInfo);
+                        process.WaitForInputIdle();
+                    }
+
+                    var recordId = match.Groups[1].Value;
+                    var recordFileName = Path.Combine(recordDirectory, recordId + ".recording");
+                    var destinationFileName = Path.Combine(outputFolder, recordId + ".recording");
+
+                    if (File.Exists(destinationFileName))
+                    {
+                        Console.WriteLine("Skiping record " + recordId + ".");
+                        continue;
+                    }
+
+                    var timeoutTime = DateTime.Now.AddSeconds(20);
+                    Console.WriteLine("Sending record " + recordId + " to Tibiacast Client.");
+
+                    Process.Start(tibiacastClientFileName, "tibiacast:recording:" + recordId);
+
+                    while (timeoutTime > DateTime.Now)
+                    {
+                        if (File.Exists(recordFileName))
+                        {
+                            File.Move(recordFileName, destinationFileName);
+                            break;
+                        }
+
+                        Thread.Sleep(500);
+                    }
+
+                    count++;
+                }
+
+                if (count > 0)
+                {
+                    if (process != null && !process.HasExited)
+                    {
+                        Console.WriteLine("Killing the Tibiacast Client before moving to next page.");
+                        process.Kill();
+                        process = null;
+                    }
+                }
+            }
+
+            Console.WriteLine("All pages were processed.");
+
         }
     }
 }
